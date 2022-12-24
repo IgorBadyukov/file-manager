eb5511957307ca88b07e6708c60c4a77a9d295c8
Igor Badyukov <badyukov1806302003@gmail.com>
2022-12-24 23:57:15 +0300

    refactor: refactor homedir and cd message

--- src/operations/commands.js
+++ src/operations/commands.js
@@ -17,6 +17,7 @@ import {compress} from "../hash-compress/compress.js";
 import {decompress} from "../hash-compress/decompress.js";
 import { stdin as input, stdout as output, exit } from 'process';
 import readline from 'readline';
+import { showCurrentDir } from './process.js';
 
 const rl = readline.createInterface({ input, output });
 
@@ -25,39 +26,49 @@ export function writeCommand() {
         switch (answer.split(' ')[0]) {
             case 'up':
                 await up();
+                showCurrentDir();
                 break;
             case 'cd':
                 await cd(answer.split(' ')[1]);
+                showCurrentDir();
                 break;
             case 'ls':
                 await ls();
                 break;
             case 'cat':
                 await read(answer.split(' ')[1]);
+                showCurrentDir();
                 break;
             case 'add':
                 await add(answer.split(' ')[1]);
+                showCurrentDir();
                 break;
             case 'rn':
                 await rename(answer.split(' ')[1], answer.split(' ')[2]);
+                showCurrentDir();
                 break;
             case 'cp':
                 await copy(answer.split(' ')[1], answer.split(' ')[2]);
+                showCurrentDir();
                 break;
             case 'mv':
                 await move(answer.split(' ')[1], answer.split(' ')[2]);
+                showCurrentDir();
                 break;
             case 'rm':
                 await remove(answer.split(' ')[1]);
+                showCurrentDir();
                 break;
             case 'hash':
                 await hash(answer.split(' ')[1]);
                 break;
             case 'compress':
                 await compress(answer.split(' ')[1], answer.split(' ')[2]);
+                showCurrentDir();
                 break;
             case 'decompress':
                 await decompress(answer.split(' ')[1], answer.split(' ')[2]);
+                showCurrentDir();
                 break;
             case '.exit':
                 exit();
@@ -66,18 +77,23 @@ export function writeCommand() {
                 switch(answer.split(' ')[1]) {
                     case '--eol':
                         getEol();
+                        showCurrentDir();
                         break;
                     case '--cpus':
                         cpus();
+                        showCurrentDir();
                         break;
                     case '--homedir':
                         homedir();
+                        showCurrentDir();
                         break;
                     case '--username':
                         username();
+                        showCurrentDir();
                         break;
                     case '--architecture':
-                        architecture()
+                        architecture();
+                        showCurrentDir();
                         break;
                     default:
                         console.error('Invalid input (parameters)');
--- src/operations/process.js
+++ src/operations/process.js
@@ -1,11 +1,13 @@
 import process, { cwd } from 'process';
 import { exit } from 'process';
+import { homedir } from 'os'
 
 let userName = '';
 
 export function processCheck() {
     if (process.argv[2].slice(0, 10) === '--username') {
         userName = process.argv[2].slice(11);
+        process.chdir(homedir());
         console.log(`Welcome to the File Manager, ${userName}!`);
         showCurrentDir();
     }
@@ -16,6 +18,6 @@ export function processCheck() {
     });
 }
 
-function showCurrentDir() {
+export function showCurrentDir() {
     console.log(`You are currently in ${cwd()}`);
 }
